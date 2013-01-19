7b7c464e0adbb03941505cee9fec497bccadd829
Viktor Turskyi <viktor@webbylab.com>
2013-01-19 20:37:51 +0200

    Added "Special rules"

--- lib/Validator/LIVR/Rules/Special.pm
+++ lib/Validator/LIVR/Rules/Special.pm
@@ -3,4 +3,30 @@ package Validator::LIVR::Rules::Special;
 use strict;
 use warnings;
 
+use Email::Valid;
+
+sub email {
+    return sub {
+        my $value = shift;
+        return if !defined($value) || $value eq '';
+
+        return 'WRONG_EMAIL' unless Email::Valid->address($value);
+        return;
+    };
+}
+
+
+sub equal_to_field {
+    my $field = shift;
+
+    return sub {
+        my ( $value, $params ) = @_;
+        return if !defined($value) || $value eq '';
+
+        return 'FIELDS_NOT_EQUAL' unless $value eq $params->{$field};
+        return;
+    };
+
+}
+
 1;
